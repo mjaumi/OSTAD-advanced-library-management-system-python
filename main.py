97b3b4689faec06244a791f3b79f5b88873c5985
mjaumi <mjaumi2864@gmail.com>
2024-12-12 21:11:30 +0600

    initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+book_list = []
+
+while True:
+    print('Welcome To Library Management System!!')
+    print('0. Exit')
+    print('1. Add A Book')
+    print('2. Update A Book')
+    print('3. Delete A Book')
+    print('4. View All Books')
+
+    option = input('\nChoose An Option: ')
+
+    if option == '0':
+        print('\n----------------------------------------')
+        print('EXITING LIBRARY MANAGEMENT SYSTEM')
+        print('----------------------------------------\n')
+
+        print('Thank You For Using Library Management System!!\n')
+        break
+
+    elif option == '1':
+        pass
+
+    else:
+        print('Please, Select A Valid Option!!\n')
